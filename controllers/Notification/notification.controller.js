4d70a2b26ef71d6f3105b9e819a7abb382e281e0
DevManishR <manishrawat0711@gmail.com>
2024-09-12 18:38:14 +0530

    Get all Notification api Implemented

--- controllers/Notification/notification.controller.js
+++ controllers/Notification/notification.controller.js
@@ -1,3 +1,4 @@
+import { successResponseWithData } from "../../helpers/apiResponse.js"
 import notificationModel from "../../models/Notification/notification.model.js"
 export const createNotification=async(req,res)=>{
     try{
@@ -16,5 +17,24 @@ export const createNotification=async(req,res)=>{
     }catch(error){
 
         console.log(error)
+    }
+}
+
+export const allNotification=async(req,res)=>{
+    const userid = req.userid;
+
+     //console.log("no id",id)
+    try{
+               
+        const AllNotification=await notificationModel.find({userid:userid})
+
+        console.log("AllNotification",AllNotification)
+
+        return(
+            successResponseWithData(res,"All Notification fetched Sucessfully",AllNotification)
+        )
+    
+    }catch(error){
+
     }
 }
--- controllers/Notification/notification.route.js
+++ controllers/Notification/notification.route.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { createNotification } from "./notification.controller.js"
+import { allNotification, createNotification } from "./notification.controller.js"
 import { CheckHeaderToken } from "../../helpers/authHelper.js"
 
 const app=express.Router()
 
 
 app.post('/create-notification',CheckHeaderToken,createNotification)
+app.get('/get-all-notification/:id',CheckHeaderToken,allNotification)
 
 export default app
