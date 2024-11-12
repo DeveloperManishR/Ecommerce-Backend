a790f8e9b1a9314acbb94cfca1ef8d9647a6f868
DeveloperManishR <manishrawat0711@gmail.com>
2024-11-12 11:43:14 +0530

    test chnage

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(
 //app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.urlencoded({ extended: false, limit: 1000 }));
-app.use(cors("*"));
+app.use(cors("*")) ;
 
 app.use("/api", allRoutes);
 app.use('/public', express.static("public"));
