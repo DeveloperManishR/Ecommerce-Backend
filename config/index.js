2c32d42eccff13fbb0bc7b1bce8149738c776a16
DeveloperManishR <manishrawat0711@gmail.com>
2025-04-28 19:28:10 +0530

    sdfdsf

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 export const ConnectDB=async()=>{
    try{
-   await mongoose.connect("mongodb+srv://Manish:f54PbTgjvDVAtyzr@cluster0.nfxrx.mongodb.net/ecommerce")
+   await mongoose.connect("mongodb://root:nDdWTmSemEhYtVF2e3xbsXGH0OZofdga6QXMeTcv4OzC5wW0keY8OZlDwXjUoh2u@82.29.161.196:5432/?directConnection=true")
    }catch(error){
     console.log("Error connection...", error);
    }
